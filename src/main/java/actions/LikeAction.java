ea1651bc886620dcb4acaf3149bfe0ecf8c7f83b
mito <mito@NC2305-09>
2024-03-05 10:43:51 +0900

    いいね機能エラー状態

---
+++ src/main/java/actions/LikeAction.java
@@ -0,0 +1,43 @@
+package actions;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import actions.views.EmployeeView;
+import actions.views.LikeView;
+import actions.views.ReportView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import services.LikeService;
+
+public class LikeAction extends ActionBase {
+    private LikeService service;
+
+    @Override
+    public void process() throws ServletException, IOException {
+        service = new LikeService();
+
+        //メソッドを実行
+        invoke();
+        service.close();
+    }
+
+    public void create() throws ServletException, IOException {
+
+        //セッションからログイン中の従業員情報を取得
+        EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+        ReportView rv = service.findOne(Integer.valueOf(getRequestParam(AttributeConst.REP_ID)));
+
+        LikeView lv = new LikeView(
+                null,
+                ev,
+                rv,
+                null,
+                null);
+
+        service.create(lv);
+        redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
+
+    }
+}
--- src/main/java/services/LikeService.java
+++ src/main/java/services/LikeService.java
@@ -4,16 +4,38 @@ import java.time.LocalDateTime;
 
 import actions.views.LikeConverter;
 import actions.views.LikeView;
+import actions.views.ReportConverter;
+import actions.views.ReportView;
+import models.Report;
 
 //いいねテーブルの操作に関わる処理を行うクラス
 
 public class LikeService extends ServiceBase {
-    private void create(LikeView lv) {
+
+    public ReportView findOne(int id) {
+        return ReportConverter.toView(findOneInternal(id));
+    }
+
+    public void create(LikeView lv) {
         LocalDateTime ldt = LocalDateTime.now();
         lv.setCreatedAt(ldt);
         lv.setUpdatedAt(ldt);
+        createInternal(lv);
+    }
+
+    private Report findOneInternal(int id) {
+        return em.find(Report.class, id);
+    }
+
+    /**
+     * いいねデータを1件登録する
+     * @param lv いいねデータ
+     */
+    private void createInternal(LikeView lv) {
+
         em.getTransaction().begin();
         em.persist(LikeConverter.toModel(lv));
         em.getTransaction().commit();
+
     }
 }
