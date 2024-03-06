f9639cebcafc588f2f63683e1d59d958e805cdb5
mito <mito@NC2305-09>
2024-03-06 16:37:30 +0900

    コメントの追記

--- src/main/java/services/LikeService.java
+++ src/main/java/services/LikeService.java
@@ -12,10 +12,19 @@ import models.Report;
 
 public class LikeService extends ServiceBase {
 
+    /**
+     * idを条件に取得したデータをReportViewのインスタンスで返却する
+     * @param id
+     * @return 取得データのインスタンス
+     */
     public ReportView findOne(int id) {
         return ReportConverter.toView(findOneInternal(id));
     }
 
+    /**
+     * 画面の日報情報を元にいいねのデータを1件作成し、いいねテーブルに登録する
+     * @param lv いいねの登録内容
+     */
     public void create(LikeView lv) {
         LocalDateTime ldt = LocalDateTime.now();
         lv.setCreatedAt(ldt);
@@ -23,6 +32,11 @@ public class LikeService extends ServiceBase {
         createInternal(lv);
     }
 
+    /**
+     * idを条件にデータを1件取得する
+     * @param id
+     * @return 取得データのインスタンス
+     */
     private Report findOneInternal(int id) {
         return em.find(Report.class, id);
     }
